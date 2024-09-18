0bb7b1de0bd3117c08e231c2e723ff23ee33b26c
4llves <jhonatab4@gmail.com>
2024-09-18 18:28:36 -0300

    feat: Implement username claiming form

--- src/pages/home/ClaimUsernameForm/index.tsx
+++ src/pages/home/ClaimUsernameForm/index.tsx
@@ -1,11 +1,33 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { Form } from "./styles";
 
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+});
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>;
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>();
+
+  async function handlerClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data);
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handlerClaimUsername)}>
+      <TextInput
+        {...register("username")}
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuario"
+        crossOrigin=""
+        onPointerEnterCapture=""
+        onPointerLeaveCapture=""
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
