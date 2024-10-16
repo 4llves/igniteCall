704cef24a20f1b905f0b7215458b4403956d701d
4llves <jhonatab4@gmail.com>
2024-10-16 17:53:45 -0300

    feat(calendar): Add navigation and current month/year display

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,23 +8,48 @@ import {
   CalendarTitle,
 } from "./styles";
 import { getWeekDays } from "@/utils/get-week-days";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set("date", 1);
+  });
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, "month");
+
+    setCurrentDate(previousMonthDate);
+  }
+
+  function handleNextMonth() {
+    const nextMonthDate = currentDate.add(1, "month");
+
+    setCurrentDate(nextMonthDate);
+  }
+
   const shortWeekDays = getWeekDays({ short: true });
+
+  const currentMonth = currentDate.format("MMMM");
+  const currentYear = currentDate.format("YYYY");
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Outubro <span>2024</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button type="button">
+          <button
+            title="Previous month"
+            type="button"
+            onClick={handlePreviousMonth}
+          >
             <ChevronLeft />
           </button>
 
-          <button type="button">
+          <button title="Next month" type="button" onClick={handleNextMonth}>
             <ChevronRight />
           </button>
         </CalendarActions>
