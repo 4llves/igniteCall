daebfbda7e51a52a2fa76ede89d66708e1bf03ce
4llves <jhonatab4@gmail.com>
2024-11-05 19:03:05 -0300

    Fix: Correctly identify blocked dates in scheduling

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -44,24 +44,25 @@ export default async function handle(
 
   const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
     SELECT
-      EXTRACT(DAY FROM S.date) AS date,
+      EXTRACT(DAY FROM S.DATE) AS date,
       COUNT(S.date) AS amount,
       ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+
     FROM schedulings S
 
     LEFT JOIN user_time_intervals UTI
-      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))      
-    
+      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
+
     WHERE S.user_id = ${user.id}
-      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+     AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
 
     GROUP BY EXTRACT(DAY FROM S.date),
-    ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
 
-    HAVING amount >= size      
+    HAVING amount >= size
   `
 
-  const blockedDates = blockedDatesRaw.map((item) => item.date)
+  const blockedDates = blockedDatesRaw.map((item) => Number(item.date))
 
   return res.json({ blockedWeekDays, blockedDates })
 }
